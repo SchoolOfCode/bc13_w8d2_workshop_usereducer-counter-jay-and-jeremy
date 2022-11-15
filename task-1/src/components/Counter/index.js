480b41d60786fceb19a2d6c773a4337d20476ca1
jamiestungo <jamiestungo@gmail.com>
2022-11-15 15:17:43 +0000

    Task 1

--- task-1/src/components/Counter/index.js
+++ task-1/src/components/Counter/index.js
@@ -1,23 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
+
+function reducer(state, action) {
+  switch(action) {
+    case 'ADD':
+      return state += 1;
+    case 'DEL':
+      return state -= 1;
+    default:
+    return state;
+  }
+}
 
 function Counter() {
-  const [count, setCount] = useState(0);
-
-  function incrementCount() {
-    const updatedCount = count + 1;
-    setCount(updatedCount);
-  }
-
-  function decrementCount() {
-    const updatedCount = count - 1;
-    setCount(updatedCount);
-  }
+  const [count, dispatch] = useReducer(reducer, 0);
+ // const [count, setCount] = useState(0);
 
   return (
     <div>
-      <button onClick={incrementCount}>+</button>
+      <button onClick={() => {dispatch('ADD')}}>+</button>
       <p>{count}</p>
-      <button onClick={decrementCount}>-</button>
+      <button onClick={() => {dispatch('DEL')}}>-</button>
     </div>
   );
 }
